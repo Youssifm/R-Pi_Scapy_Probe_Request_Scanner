2c2d7976b952a98050ae1c99d106339c87cfb0d5
Youssifm <Yoma1993@gmail.com>
2019-02-20 08:42:54 -0600

    Update Scapy-MySQL-Script.py

--- Scapy-MySQL-Script.py
+++ Scapy-MySQL-Script.py
@@ -12,27 +12,27 @@
 #   Version:    1.0                                                                             #
 #                                                                                               #
 #   Description:                                                                                #
-#																								#
-#       This script uses the Scapy python API to sniff probe requests then						#
-#	uploads the probe request to a SQL server. The script is split into							#
-#	four threads. The main thread the three "worker" threads that do the						#
-#	heavy lifting. The Scapy worker thread sniffs for probe request and							#
-#	passes the probe requests to the data handler. The data handler								#
-#	puts the data into a dictionary (aka hash table) to hold the data for						#
-#	the specified WINDOW_TIME. This decreases the amount of traffic to the						#
-#	SQL server. Holding the data also enables us to account for duplicates						#
-#	within the specified WINDOW_TIME. After the specified WINDOW_TIME the						#
-#	is passes to the SQL thread to be uploaded to the SQL database.								#
+#												#
+#       This script uses the Scapy python API to sniff probe requests then			#
+#	uploads the probe request to a SQL server. The script is split into			#
+#	four threads. The main thread the three "worker" threads that do the			#
+#	heavy lifting. The Scapy worker thread sniffs for probe request and			#
+#	passes the probe requests to the data handler. The data handler				#
+#	puts the data into a dictionary (aka hash table) to hold the data for			#
+#	the specified WINDOW_TIME. This decreases the amount of traffic to the			#
+#	SQL server. Holding the data also enables us to account for duplicates			#
+#	within the specified WINDOW_TIME. After the specified WINDOW_TIME the			#
+#	is passes to the SQL thread to be uploaded to the SQL database.				#
 #                                                                                               #
 #   Notes:                                                                                      #
 #                                                                                               #
 #   Issues:                                                                                     #
 #                                                                                               #
-#   Change Log:  																				#
-#		                                                                                		#
+#   Change Log:  										#
+#		                                                                                #
 #       v1.0 (02/06/2017)                                                                       #
-#			1. First iteration		                                               				#
-#																								#
+#			1. First iteration		                                        #
+#												#
 # --------------------------------------------------------------------------------------------- #
 
 import datetime
